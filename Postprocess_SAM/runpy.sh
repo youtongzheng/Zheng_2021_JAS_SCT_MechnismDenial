9afa7d0f546a4f63ad366351f72c8fa9320befbb
Youtong Zheng <zhengyoutong@gmail.com>
2021-04-01 15:22:03 -0400

    add the Postprocess procedure of SAM output

---
+++ Postprocess_SAM/runpy.sh
@@ -0,0 +1,4 @@
+python Postprocess_SAM.py '/lustre/ytzheng/' 'ASTEX_cold2.6'
+python Postprocess_SAM.py '/lustre/ytzheng/' 'ASTEX_cold2.6_fxdlhf'
+python Postprocess_SAM.py '/lustre/ytzheng/' 'ASTEX_cold2.6_240x240'
+python Postprocess_SAM.py '/lustre/ytzheng/' 'ASTEX_cold2.6_fxdlhf_240x240'
